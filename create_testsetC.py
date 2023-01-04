b926db2e76923aea1fd1c0cbdcf60d13cfafe7b4
TimJaspers <55140275+TimJaspers@users.noreply.github.com>
2023-01-04 16:15:40 +0100

    update

--- create_testsetC.py
+++ create_testsetC.py
@@ -3,13 +3,21 @@ from utils import define_corruption
 import random
 from utils import Compose, combine_paths
 from PIL import Image
+import pandas as pd
+import sys
 
 random.seed(0)
+
+
 def create_testset_c(path_images, path_masks=None):
     """read images and check imagesize"""
-
     cwd = os.getcwd()
     images = os.listdir(path_images)
+
+    """
+    we create an excel file to link all corrupted images to the orginal ones to link them later with the correct label
+    """
+    df = pd.DataFrame()
 
     print('total number of images in folder = ' + str(len(images)))
     combined_path_images = combine_paths(path_images, images)
@@ -23,57 +31,57 @@ def create_testset_c(path_images, path_masks=None):
 
     print('total number of images removed because of low resolution = ' + str(len(idx_remove)))
     path_images_clean = [i for j, i in enumerate(combined_path_images) if j not in idx_remove]
+    images_clean = [i for j, i in enumerate(images) if j not in idx_remove]
     print('total number of images used for TestSet-C = ' + str(len(path_images_clean)))
 
+    df["original file"] = images_clean
+
     """perturbations"""
-    corruption_options = ['Resolution', 'JPG', 'SpotLight - light', 'Contrast', 'Brightness',
-                            'Saturation', 'Hue', 'defocus-blur', 'Motion-blur']
+    corruption_options = ['Resolution', 'JPG', 'SpotLight - light', 'Contrast', 'Brightness'
+                           ,'Saturation', 'Hue', 'defocus-blur', 'Motion-blur']
 
     factor_1 = [1,2,3,4,5]
     factor_2 = [6,7,8,9,10,11,12,13,14,15]
 
     # save dir
-    path_save_images = os.path.join(cwd,'Testset-C/Images')
+    path_save_images = os.path.join(cwd, 'Testset-C/Images')
     os.makedirs(path_save_images, exist_ok=True)
 
-    if path_masks != None:
-        path_save_masks = os.path.join(cwd,'Testset-C/Masks')
+    if path_masks is not None:
+        path_save_masks = os.path.join(cwd, 'Testset-C/Masks')
         os.makedirs(path_save_masks, exist_ok=True)
 
-        masks = os.listdir((path_masks))
+        masks = os.listdir(path_masks)
         combined_path_masks = combine_paths(path_masks, masks)
         path_masks_clean = [i for j, i in enumerate(combined_path_masks) if j not in idx_remove]
 
         if len(path_masks_clean) != len(path_images_clean):
             AttributeError('not the same amount of images and masks')
 
-
-
-
-    # corruptions
-    nb_it = 5 #number of iteration over testset
-    nb_per = 5 #maximum number of corruptions per images
+    # apply corruptions
+    nb_it = 5  # number of iteration over testset
+    nb_per = 5  # maximum number of corruptions per images
 
     for i in list(range(nb_it)):
+        names = []
         for k in range(len(path_images_clean)):
             '''resize images to 1024x1024'''
 
             image = Image.open(path_images_clean[k])
-            image.resize((1024,1024),resample=Image.LANCZOS)
+            image = image.resize((1024, 1024), resample=Image.LANCZOS)
 
-            nb_corruptions = random.randint(1,nb_per) #random number of corruptions for image
-            idx_corruptions = random.sample(range(0, len(corruption_options)), nb_corruptions) #random chosen corruption
+            nb_corruptions = random.randint(1,nb_per)  # random number of corruptions for image
+            idx_corruptions = random.sample(range(0, len(corruption_options)), nb_corruptions)  # random chosen corruption
 
             corruptions = list()
             corruptions_names = list()
             factors = list()
 
-            #create list with all perturbations
+            # create list with all perturbations
             for nb in range(nb_corruptions):
-                #append perturbation to transform
+                # append perturbation to transform
                 corruption = corruption_options[idx_corruptions[nb]]
                 corruptions_names.append(corruption)
-
 
                 if corruption in ['Contrast', 'Brightness','Saturation', 'Hue']:
                     factor = factor_2[random.randint(0,len(factor_2)-1)]
@@ -83,32 +91,46 @@ def create_testset_c(path_images, path_masks=None):
                 corruptions.extend(define_corruption(corruption, factor=factor))
                 factors.append(factor)
 
-            #apply perturbations on images
+            # apply perturbations on images
             transforms = Compose(corruptions)
             image_c = transforms(image)
 
             name = 'image_' + str(random.randint(0,10000)) + '_'
-            for i in range(len(corruptions_names)):
-                name = name + corruptions_names[i] + '_' + str(factors[i]) + '_'
+            for j in range(len(corruptions_names)):
+                name = name + corruptions_names[j] + '_' + str(factors[j]) + '_'
 
             name = name + '.png'
+            names.append(name)
             path_image = os.path.join(path_save_images, name)
             image_c.save(path_image)
 
-            if path_masks != None:
+            if path_masks is not None:
                 mask = Image.open(path_masks_clean[k])
                 mask.resize((1024, 1024), resample=Image.NEAREST)
                 path_mask = os.path.join(path_save_masks, name)
                 mask.save(path_mask)
+
+        df["iteration " + str(i)] = names
+
         print('iteration ' + str(i) + ' is done')
-        print(str(nb_it-i) + ' iterations to go')
+        print(str(nb_it-(i+1)) + ' iterations to go')
         print('-----------------------------------')
 
+    df.to_excel(os.path.join(cwd, 'conversion_file.xlsx'))
     print('Testset-C is created!')
+    print('In case of classification the conversion file can be used to link the correct label to the corrupted images')
+    print('In case of segmentation the ground truth masks got the same name as the corrupted images')
+
 
 if __name__ == "__main__":
-    path_images = r'C:\Users\20172619\OneDrive - TU Eindhoven\PhD\jaar 1\first_experiments\Testset-C\val-set-images'
-    path_masks = r'C:\Users\20172619\OneDrive - TU Eindhoven\PhD\jaar 1\first_experiments\Testset-C\val-set-masks'
+
+    path_images = sys.argv[1]
+    # Check if there are at least two inputs
+    if len(sys.argv) >= 3:
+        # If there are, set input2 to the second input
+        path_masks = sys.argv[2]
+    else:
+        path_masks = None
 
     create_testset_c(path_images, path_masks=path_masks)
 
